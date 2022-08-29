ea346e6667c63964d63dc350be242868bfc87bb1
sardor <109537648+djurakulov14@users.noreply.github.com>
2022-08-29 19:34:19 +0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -8,30 +8,27 @@ let photo = document.querySelector('.photo')
 console.log(photo);
 
 btn1.onclick = () => {
-    // photo.classList.remove('photo')
-    // photo.classList.add('photo1')
-    photo.classList.replace('photo', 'photo1')
+    photo.classList.remove('photo','photo1','photo2','photo3','photo4','photo5')
+    photo.classList.add('photo1')
 }
 btn2.onclick = () => {
-    // photo.classList.remove('photo')
-    // photo.classList.add('photo2')
-    photo.classList.replace('photo1' , 'photo2')
+    photo.classList.remove('photo','photo1','photo2','photo3','photo4','photo5')
+    photo.classList.add('photo2')
+
 
 }
 btn3.onclick = () => {
-    // photo.classList.remove('photo')
-    photo.classList.replace('photo2', 'photo3')
-    // photo.classList.add('photo3')
+    photo.classList.remove('photo','photo1','photo2','photo3','photo4','photo5')
+
+    photo.classList.add('photo3')
 }
 btn4.onclick = () => {
-    // photo.classList.remove('photo')
-    // photo.classList.add('photo4')
-    photo.classList.replace('photo3', 'photo4')
+    photo.classList.remove('photo','photo1','photo2','photo3','photo4','photo5')
+    photo.classList.add('photo4')
 
 }
 btn5.onclick = () => {
-    // photo.classList.remove('photo')
-    // photo.classList.add('photo5')
-    photo.classList.replace('photo4', 'photo5')
+    photo.classList.remove('photo','photo1','photo2','photo3','photo4','photo5')
+    photo.classList.add('photo5')
 
 }
